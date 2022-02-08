a32efa4ebebf7c7cfa2a7c2e07404f8e16099c98
chrispalmo <34981948+chrispalmo@users.noreply.github.com>
2022-02-08 22:12:56 +1000

    create .config/nvim dir if it doesn't exist

--- install.sh
+++ install.sh
@@ -1,16 +1,17 @@
 #!/bin/sh
 
 # make symlinks for dotfiles
-[ ! -e ~/.config/nvim ] && ln -s ~/.files/.config/nvim ~/.config/nvim
-[ ! -e ~/.tmux.conf   ] && ln -s ~/.files/.tmux.conf   ~/.tmux.conf
-[ ! -e ~/.vim         ] && ln -s ~/.files/.vim         ~/.vim
+[ ! -e ~/.config      ] && mkdir ~/.config
+[ ! -e ~/.config/nvim ] && ln -s ~/.files/.config/nvim          ~/.config/nvim
+[ ! -e ~/.tmux.conf   ] && ln -s ~/.files/.tmux.conf            ~/.tmux.conf
+[ ! -e ~/.vim         ] && ln -s ~/.files/.vim                  ~/.vim
 [ ! -e ~/.vimrc       ] && ln -s ~/.files/.config/nvim/init.vim ~/.vimrc
-[ ! -e ~/.zshrc       ] && ln -s ~/.files/.zshrc       ~/.zshrc
+[ ! -e ~/.zshrc       ] && ln -s ~/.files/.zshrc                ~/.zshrc
 
 # install tmux plugins
 [ ! -e ~/.tmux/plugins/tpm ] &&
    git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm &&
-    ~/.tmux/plugins/tpm/bindings/install_plugins
+      ~/.tmux/plugins/tpm/bindings/install_plugins
 
 # install vundle
 [ ! -e ~/.tmux/plugins/Vundle.vim ] &&
