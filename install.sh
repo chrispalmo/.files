4dc00c63d8c1a846b0fc6dc7f541564f11b5a1e2
chrispalmo <34981948+chrispalmo@users.noreply.github.com>
2022-02-14 20:49:56 +1000

    add karabiner config

--- install.sh
+++ install.sh
@@ -1,12 +1,14 @@
 #!/bin/sh
 
 # make symlinks for dotfiles
-[ ! -e ~/.config      ] && mkdir ~/.config
-[ ! -e ~/.config/nvim ] && ln -s ~/.files/.config/nvim          ~/.config/nvim
-[ ! -e ~/.tmux.conf   ] && ln -s ~/.files/.tmux.conf            ~/.tmux.conf
-[ ! -e ~/.vim         ] && ln -s ~/.files/.vim                  ~/.vim
-[ ! -e ~/.vimrc       ] && ln -s ~/.files/.config/nvim/init.vim ~/.vimrc
-[ ! -e ~/.zshrc       ] && ln -s ~/.files/.zshrc                ~/.zshrc
+[ ! -e ~/.config           ] && mkdir ~/.config
+[ -e ~/.config/karabiner   ] && mv ~/.config/karabiner           ~/.config/karabiner.backup
+[ ! -e ~/.config/karabiner ] && ln -s ~/.files/.config/karabiner ~/.config/karabiner
+[ ! -e ~/.config/nvim ] && ln -s ~/.files/.config/nvim           ~/.config/nvim
+[ ! -e ~/.tmux.conf   ] && ln -s ~/.files/.tmux.conf             ~/.tmux.conf
+[ ! -e ~/.vim         ] && ln -s ~/.files/.vim                   ~/.vim
+[ ! -e ~/.vimrc       ] && ln -s ~/.files/.config/nvim/init.vim  ~/.vimrc
+[ ! -e ~/.zshrc       ] && ln -s ~/.files/.zshrc                 ~/.zshrc
 
 # install tmux plugins
 [ ! -e ~/.tmux/plugins/tpm ] &&
